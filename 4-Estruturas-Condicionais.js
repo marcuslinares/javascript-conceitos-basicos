731d58b7c4b81c4d1b8fcfd37fb291f06ea804dd
Marcus Linares <marcus.linares@gmail.com>
2020-09-12 14:43:19 -0300

    Adicionando arquivo de Estruturas Condicionais

---
+++ 4-Estruturas-Condicionais.js
@@ -0,0 +1,10 @@
+let nomeUsuario = 'Marcus linares';
+let mensagem = null;
+
+if (nomeUsuario == 'Marcus Linares'){
+    mensagem = 'Nome igual';
+} else {
+    mensagem = 'Nome diferente';
+}
+
+console.log(mensagem);
